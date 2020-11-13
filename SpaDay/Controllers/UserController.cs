8dae2502c32899f5700ec4d23a1f7c9c2fd1d2cb
Kristian <jettkristian@gmail.com>
2020-11-12 19:52:02 -0800

    Completed studio without bonus missions

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(string username, string email, string password, string verify)
+        {
+            User newUser = new User(username, email, password);
+            if (newUser.Password == verify)
+            {
+                ViewBag.User = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.userName = username;
+                ViewBag.email = email;
+                ViewBag.error = "Passwords did not match";
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+
+        public int Id;
+        static int nextId = 1;
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; }
+
+        public User()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public User(string username, string email, string password) : this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
